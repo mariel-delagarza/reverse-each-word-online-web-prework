6799408b58122639751a02b93c740115e0ff7361
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2018-11-21 22:22:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(string)
-  words = string.split
+def reverse_each_word(sentence)
+  words = sentence.split
   words.each do |word|
     word.reverse!
   end
-  words.join
+  words.join(" ")
 end 
